fb8284a2ffa6a58630bdb01675a8907b617aa427
Oscar-CR <krz.oficial@gmail.com>
2023-10-31 15:21:29 -0600

    agregado de base de base de datos

--- database/migrations/2023_10_31_175358_medicals.php
+++ database/migrations/2023_10_31_175358_medicals.php
@@ -13,7 +13,55 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('medicals', function (Blueprint $table) {
+            $table->id();
+            $table->string('degree');
+            $table->string('phone');
+            $table->string('status', 20)->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+
+        Schema::create('user_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->string('url');
+            $table->string('more_information');
+            $table->string('status',20);
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+
+        Schema::create('event_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('event_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('status',20);
+            $table->timestamps();
+        });
+
+        Schema::create('event_invited', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('event_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +71,10 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('user_logs');
+        Schema::dropIfExists('event_logs');
+        Schema::dropIfExists('event_invited');
+        Schema::dropIfExists('medicals');
+        Schema::dropIfExists('events');
     }
 };
