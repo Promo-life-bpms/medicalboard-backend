fc5524ca2f37bcc37cc9ce7d8b35ae83ba0ec940
Carlos180200 <reyesmartinezcarlos2000@hotmail.com>
2023-11-14 09:43:13 -0600

    tabla dinamica

---
+++ app/Http/Livewire/Tabladinamica.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Event;
+use App\Models\EventLog;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Tabladinamica extends Component
+{
+    use WithPagination;
+
+    public $event;
+    public $logs;
+    public $userCheckin = [];
+    public $userNoCheckin = [];
+    public $usersNoInvited;
+    public $usuarios;
+    public $user;
+    public $nombres;
+    public $usuariosFiltrados;
+    public $user_checkin;
+    public $user_no_checkin;
+    public $users_no_invited;
+
+    
+
+    public function mount($id)
+    {
+        $this->event = Event::findOrFail($id);
+        $this->loadData();
+    }
+
+    public function loadData()
+    {
+        $users_invited = User::whereIn('id', json_decode($this->event->invited->users))->get();
+        $users_array = collect(json_decode($this->event->invited->users));
+
+        $user_checkin = [];
+        $user_no_checkin = [];
+
+        $users_logs = EventLog::where('event_id', $this->event->id)->where('status', 1)->pluck('user_id')->toArray();
+
+        $users_no_invited = EventLog::where('event_id', $this->event->id)->where('status', 0)->get();
+
+        $users_logs_collection = collect($users_logs);
+
+        foreach ($users_invited as $user) {
+            if ($users_logs_collection->contains($user->id)) {
+                array_push($user_checkin, $user);
+            } else {
+                array_push($user_no_checkin, $user);
+            }
+        }        
+
+        $this->userCheckin = $user_checkin;
+        $this->userNoCheckin = $user_no_checkin;
+        $this->usersNoInvited = $users_no_invited;
+
+        $this->user = auth()->user();
+        $this->usuarios = User::all();
+
+        $existingUserIds = DB::table('event_invited')
+            ->where('event_id', $this->event->id)
+            ->pluck('users')
+            ->flatMap(function ($users) {
+                return json_decode($users);
+            })->toArray();
+
+        $names = [];
+        foreach ($existingUserIds as $userId) {
+            $user = DB::table('users')->where('id', $userId)->first();
+            if ($user) {
+                $names[] = $user->name . ' ' . $user->lastname;
+            }
+        }
+
+        $this->nombres = implode(", ", $names);
+
+        $this->usuariosFiltrados = $this->usuarios->reject(function ($usuario) use ($existingUserIds) {
+            return in_array($usuario->id, $existingUserIds);
+        });
+        
+        $this->logs = $this->event->logs;
+
+
+    }
+
+    public function render()
+    {
+        return view('livewire.tabladinamica');
+    }
+
+    public function updateLogs()
+    {
+        $this->loadData(); // Llama a la lógica de carga de datos
+        $this->emit('logsUpdated'); // Emite el evento de logs actualizados
+
+       
+        
+    }
+}
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -106,116 +106,9 @@
 
         <div class="mt-5"></div>
 
-        <div class="flex justify-between">
-            <p class="pt-4  text-xl font-bold ">Lista de asistentes</p>
-            
-            <!-- Modal toggle -->
-            <button data-modal-target="static-modal" data-modal-toggle="static-modal" class="block text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                Agregar usuario
-            </button>
-            <!-- Main modal -->
-            <div id="static-modal"  tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div class="relative w-full max-w-2xl max-h-full">
-                    <!-- Modal content -->
-                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <!-- Modal header -->
-                        <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                                Agregar invitado
-                            </h3>
-                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal">
-                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                </svg>
-                                <span class="sr-only">Close modal</span>
-                            </button>
-                        </div>
-                        <!-- Modal body -->
-                        <div class="p-6 space-y-6">
-                            <form method="POST" action="{{ route('user.event') }}">
-                                @csrf
-                                <div class="grid gap-4 mb-4 md:grid-cols-1">
-                                    <div>
-                                        <input type="hidden" name="event_id" value="{{ $event->id }}">
-                                        <label for="created_by" style="display: none;">Usuario:</label><p style="display: none;">{{ $user->id }}</p>
-                                    </div>
+        @livewire('tabladinamica', ['id' => $event->id])
 
-                                    <div>
-                                        <label for="usuarios">Usuarios ya invitados:</label>
-                                        <p> {{$nombres}} </P>     
-                                    </div>
-
-                                    <label for="users">Selecciona un usuario</label>
-
-                                    <button id="dropdownSearchButton" data-dropdown-toggle="dropdownSearch" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Usuarios<svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/></svg>
-                                    </button>
-
-                                    <!-- Dropdown menu -->
-                                    <div id="dropdownSearch" class="z-10 hidden bg-white rounded-lg shadow w-60 dark:bg-gray-700">
-                                        <div class="p-3">
-                                            <label for="input-group-search" class="sr-only">Search</label>
-                                            <div class="relative">
-                                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                                    </svg>
-                                                </div>
-                                                <input type="text" id="input-group-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search user">
-                                            </div>
-                                        </div>
-                                        <ul class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownSearchButton">
-                                            @foreach($usuariosFiltrados as $usuario)
-                                                <li>
-                                                    <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                                                        <input name="users[]" type="checkbox" value="{{ $usuario->id }}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                                        <label for="checkbox-item-{{ $usuario->id }}" class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">{{$usuario->name.' '.$usuario->lastname}}</label>
-                                                    </div>
-                                                </li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
-
-                                    <div class="text-right">
-                                        <button type="submit" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">
-                                            Agregar invitados
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <table class="table-auto w-full mt-4" >
-            <thead class="bg-stone-100 h-12 border">
-                <tr >
-                    <th style="width: 10%;">#</th>
-                    <th style="width: 50%;">Nombre</th>
-                    <th style="width: 20%;">Hora de entrada</th>
-                    <th style="width: 20%;">Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($logs as $log)
-                <tr class="border">
-                    <th class="p-2">{{$loop->iteration}}</th>
-                    <th>{{ $log->user->medical->degree . ' ' . $log->user->name . ' ' . $log->user->lastname }}</th>
-                    <th>{{ $log->created_at }}</th>
-                    <th>@if($log->status == 1 )
-                            <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Invitado</span>
-                        @else
-                            <span class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">No invitado</span>
-                        @endif
-                        
-                    </th>
-                </tr>
-                @endforeach
-           
-            </tbody>
-        </table>
+        
 
         <div class="pagination-container text-right mt-10">
             {{ $logs->links() }}
@@ -276,6 +169,7 @@ document.addEventListener('DOMContentLoaded', function() {
  
 
     @endsection
+    
 @section('styles')
 <style>
     
