69868f54747c085ea545948ce41282e9165e595b
Oscar-CR <krz.oficial@gmail.com>
2024-01-23 21:53:15 -0600

    agregado de status multiple en tabla de datos

--- resources/views/livewire/tabladinamica.blade.php
+++ resources/views/livewire/tabladinamica.blade.php
@@ -184,9 +184,13 @@
                                     @if ($log->status == 1)
                                         <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Invitado</span>
                                     @elseif($log->status == 2)
+                                        <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Invitado</span>
                                         <span class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">No invitado</span>
+                                    @elseif($log->status == 3)
+                                        <span class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">No invitado</span>
+                                        <span class="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300">Asistencia múltiple</span>
                                     @else
-                                    <span class="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300">Asistencia múltiple</span>
+                                        <span class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">No invitado</span>
                                     @endif
                                 </th>
                             </tr>
